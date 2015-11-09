8537baa93dbb3ca74f9c0a7919845ca96c0dcc51
MErends <mark.erends@gmail.com>
2015-11-09 16:29:40 +0100

    Implementing Board and Stone

--- src/reversi/game/Board.java
+++ src/reversi/game/Board.java
@@ -1,15 +1,54 @@
 package reversi.game;
 
+import java.util.ArrayList;
+
 public class Board {
 	Stone[][] board;	
 	
 	public Board() {
 		board = new Stone[8][8];
-		board[5-1][4-1] = new Stone(Color.Black);
-		board[4-1][5-1] = new Stone(Color.Black);
-		board[4-1][4-1] = new Stone(Color.White);
-		board[5-1][5-1] = new Stone(Color.White);
+		
+		for(int x = 0; x != 8; ++x)
+			for(int y = 0; y != 8; ++y)
+				board[x][y] = new Stone(Color.None);
+		
+		board[4][3].setColor(Color.Black);
+		board[3][4].setColor(Color.Black);
+		board[3][3].setColor(Color.White);
+		board[4][4].setColor(Color.White);
 	}
 	
+	public void addStone(int x, int y, Color color) {
+		board[x][y].setColor(color);
+		flipAll(x, y, color);
+	}
+
+	private void flipAll(int x, int y, Color color) {
+		flipN(x, y, color);
+		flipNE(x, y, color);
+		flipE(x, y, color);
+		flipSE(x, y, color);
+		flipS(x, y, color);
+		flipSW(x, y, color);
+		flipW(x, y, color);
+		flipNW(x, y, color);
+	}
+
+	private void flipN(int x, int y, Color color) {
+		ArrayList<Stone> toFlip = new ArrayList<Stone>();
+		while(--x > 0) {
+			if(board[x][y].getColor() == Color.None) {
+				return;
+			} else if(board[x][y].getColor() == color) {
+				for(Stone stone : toFlip) {
+					stone.doFlip();
+				}
+				return;
+			} else {
+				toFlip.add(board[x][y]);
+			}	
+		}
+		return;
+	}
 	
 }
--- src/reversi/game/Stone.java
+++ src/reversi/game/Stone.java
@@ -4,6 +4,24 @@ public class Stone {
 	Color color;
 	
 	public Stone(Color color) {
+			this.color = color;
+	}
+
+	
+	public void doFlip() {
+		color = color == Color.Black ? Color.White : Color.Black;
+	}
+
+
+	public Color getColor() {
+		return color;
+	}
+
+
+	public void setColor(Color color) {
 		this.color = color;
 	}
+	
+	
+	
 }
