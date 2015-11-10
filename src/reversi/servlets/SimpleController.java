8fe9b0dacef506f47de7a6b994a6be09c2cc7e7a
MErends <mark.erends@gmail.com>
2015-11-10 13:17:51 +0100

    Poging tot SpringMVC te implementen

---
+++ src/reversi/servlets/SimpleController.java
@@ -0,0 +1,15 @@
+package reversi.servlets;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SimpleController {
+
+	@RequestMapping("/simple")
+	public @ResponseBody String simple() {
+		return "Hello world!";
+	}
+
+}
